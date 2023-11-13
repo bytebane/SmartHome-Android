5197368737d0e9894ffbffdf6dd30b99111b528d
ByteBane <81875670+bytebane@users.noreply.github.com>
2023-11-13 07:12:13 +0530

    multiple tweakz
    tweaks and fixes

--- app/src/main/java/com/bytebane/myhome/FirebaseCrud.kt
+++ app/src/main/java/com/bytebane/myhome/FirebaseCrud.kt
@@ -1,18 +1,17 @@
 package com.bytebane.myhome
+
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.tasks.await
 
 
 object FirebaseCrud {
-    private const val DB_URL = "https://bane-myhome-default-rtdb.asia-southeast1.firebasedatabase.app/"
-    private val database = Firebase.database(DB_URL)
+    private val database = FirebaseDatabase.getInstance()
     private val dbHomeRef = database.getReference("myHome")
     private val dbDeviceStatusRef = database.getReference("myHome/deviceStats")
     private val dbFanRef = database.getReference("myHome/fans")
@@ -20,9 +19,10 @@ object FirebaseCrud {
 
     private val liveDataFans = MutableLiveData<DataSnapshot>()
     private val liveDataSwitches = MutableLiveData<DataSnapshot>()
+    private val liveDeviceStatus = MutableLiveData<DataSnapshot>()
 
     init {
-        dbFanRef.addChildEventListener(object : ChildEventListener{
+        dbFanRef.addChildEventListener(object : ChildEventListener {
             override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
                 // Handle the child change event
                 liveDataFans.value = snapshot
@@ -37,7 +37,7 @@ object FirebaseCrud {
             override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
 
         })
-        dbSwitchRef.addChildEventListener(object : ChildEventListener{
+        dbSwitchRef.addChildEventListener(object : ChildEventListener {
             override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
                 // Handle the child change event
                 liveDataSwitches.value = snapshot
@@ -52,32 +52,50 @@ object FirebaseCrud {
             override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
 
         })
-    }
+        dbDeviceStatusRef.addChildEventListener(object : ChildEventListener {
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                // Handle the child change event
+                liveDeviceStatus.value = snapshot
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("FirebaseChildListener - ${error.code}", error.message)
+            }
+
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {}
+            override fun onChildRemoved(snapshot: DataSnapshot) {}
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+
+        })
+    }
 
 
     fun getFansData(): LiveData<DataSnapshot> {
         return liveDataFans
     }
+
     fun getSwitchesData(): LiveData<DataSnapshot> {
         return liveDataSwitches
     }
+
     //    Function To Get All Available Data from RTDB/myHome
-    suspend fun getData() : DataSnapshot {
+    suspend fun getData(): DataSnapshot {
         return dbHomeRef.get().await()
     }
+
     //    Get Device Status
-    suspend fun getDeviceStatus() : DataSnapshot {
-        return dbDeviceStatusRef.get().await()
+    fun getDeviceStatus(): LiveData<DataSnapshot> {
+//        return dbDeviceStatusRef.get().await()
+        return liveDeviceStatus
     }
 
     //    Function to update the fan speed --> takes the speed as an argument
-    suspend fun setFanSpeed(speed: Int){
+    suspend fun setFanSpeed(speed: Int) {
         dbFanRef.child("fanSpeed").setValue(speed).await()
     }
 
     //    Function to update the switch --> takes the switchName and status as an argument
-    suspend fun setSwitchStatus(switch: String, status: Boolean){
+    suspend fun setSwitchStatus(switch: String, status: Boolean) {
         dbSwitchRef.child(switch).setValue(status).await()
     }
 
--- app/src/main/java/com/bytebane/myhome/MainActivity.kt
+++ app/src/main/java/com/bytebane/myhome/MainActivity.kt
@@ -2,6 +2,8 @@ package com.bytebane.myhome
 
 import android.animation.ValueAnimator
 import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
@@ -12,21 +14,18 @@ import android.view.MenuInflater
 import android.view.View
 import android.view.animation.AlphaAnimation
 import android.view.animation.LinearInterpolator
-import android.widget.FrameLayout
-import android.widget.ImageButton
-import android.widget.ProgressBar
 import android.widget.SeekBar
-import android.widget.TextView
 import android.widget.Toast
 import android.widget.ToggleButton
 import androidx.activity.ComponentActivity
 import androidx.appcompat.widget.PopupMenu
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.OnLifecycleEvent
+import com.bytebane.myhome.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineName
@@ -34,27 +33,26 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var deviceStatus: TextView
-    private lateinit var switch1: ToggleButton
-    private lateinit var switch2: ToggleButton
-    private lateinit var switch3: ToggleButton
-    private lateinit var switch4: ToggleButton
-    private lateinit var switch5: ToggleButton
-    private lateinit var mainSwitch: ToggleButton
-    private lateinit var fanSpeed: SeekBar
-    private val fanAnimator = ValueAnimator.ofFloat(0f, 360f)
+    private lateinit var binding: ActivityMainBinding
+
+    private val dataLiveData = MutableLiveData<DataSnapshot>()
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var switchMap: Map<String, ToggleButton>
+
+    private lateinit var deviceDateTimeStr: String
 
     private lateinit var inAnimation: AlphaAnimation
     private lateinit var outAnimation: AlphaAnimation
-    private lateinit var progressBarHolder: FrameLayout
-    private lateinit var progressBar: ProgressBar
-    private lateinit var mainLayout: ConstraintLayout
-    private lateinit var noInternetView: ConstraintLayout
-    private lateinit var menuBtn: ImageButton
+    private val fanAnimator = ValueAnimator.ofFloat(0f, 360f)
 
     private val coroutineScope =
         CoroutineScope(Job() + Dispatchers.Main + CoroutineName("FireBaseRTDB"))
@@ -62,49 +60,42 @@ class MainActivity : ComponentActivity() {
         Log.e("FireBaseRTDB", exception.message.toString())
     }
 
-    private val dataLiveData = MutableLiveData<DataSnapshot>()
-
-    private lateinit var switchMap: Map<String, ToggleButton>
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-//        Views Initialization & Settings
-        progressBarHolder = findViewById(R.id.progressBarHolder)
-        noInternetView = findViewById(R.id.noInternetView)
+        firebaseAuth = FirebaseAuth.getInstance()
 
-        deviceStatus = findViewById(R.id.deviceStatus)
-        switch1 = findViewById(R.id.switch1)
-        switch2 = findViewById(R.id.switch2)
-        switch3 = findViewById(R.id.switch3)
-        switch4 = findViewById(R.id.switch4)
-        switch5 = findViewById(R.id.switch5)
-        fanSpeed = findViewById(R.id.fanSpeed)
-        mainSwitch = findViewById(R.id.mainSwitch)
-        menuBtn = findViewById(R.id.menuBtn)
-        progressBar = findViewById(R.id.progress_circular)
+        firebaseAuth.addAuthStateListener {
+            if (it.currentUser == null) {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
 
-//        Create a map of switches for convenience
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val rootView = binding.root
+        setContentView(rootView)
+
+        //        Create a map of switches for convenience
         switchMap = mapOf(
-            "switch1" to switch1,
-            "switch2" to switch2,
-            "switch3" to switch3,
-            "switch4" to switch4,
-            "switch5" to switch5
+            "switch1" to binding.switch1,
+            "switch2" to binding.switch2,
+            "switch3" to binding.switch3,
+            "switch4" to binding.switch4,
+            "switch5" to binding.switch5
         )
 
-//        Setup Animations
+        //        Setup Animations
         inAnimation = AlphaAnimation(0f, 1f)
         inAnimation.duration = 200
         outAnimation = AlphaAnimation(1f, 0f)
         outAnimation.duration = 200
 
-//        Log.d("LOG", Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION)
 
         //        Show the loader View
-        progressBarHolder.animation = inAnimation
-        progressBarHolder.visibility = View.VISIBLE
+        binding.progressBarHolder.animation = inAnimation
+        binding.progressBarHolder.visibility = View.VISIBLE
 
         showNetworkOverlay(!checkForInternet(this))
 
@@ -116,7 +107,7 @@ class MainActivity : ComponentActivity() {
         fanAnimator.repeatCount = ValueAnimator.INFINITE
         fanAnimator.interpolator = LinearInterpolator()
         fanAnimator.addUpdateListener { animation ->
-            switch5.rotation = animation.animatedValue as Float
+            binding.switch5.rotation = animation.animatedValue as Float
         }
 
 
@@ -124,32 +115,26 @@ class MainActivity : ComponentActivity() {
         coroutineScope.launch(coroutineExceptionHandler) {
             dataLiveData.value = FirebaseCrud.getData()
 
-            fanSpeed.progress =
+            binding.fanSpeed.progress =
                 dataLiveData.value!!.child("/fans/fanSpeed").value.toString().toInt()
-            switch1.isChecked = dataLiveData.value!!.child("/switches/switch1").value as Boolean
-            switch2.isChecked = dataLiveData.value!!.child("/switches/switch2").value as Boolean
-            switch3.isChecked = dataLiveData.value!!.child("/switches/switch3").value as Boolean
-            switch4.isChecked = dataLiveData.value!!.child("/switches/switch4").value as Boolean
-            switch5.isChecked = dataLiveData.value!!.child("switches/switch5").value as Boolean
+            binding.switch1.isChecked =
+                dataLiveData.value!!.child("/switches/switch1").value as Boolean
+            binding.switch2.isChecked =
+                dataLiveData.value!!.child("/switches/switch2").value as Boolean
+            binding.switch3.isChecked =
+                dataLiveData.value!!.child("/switches/switch3").value as Boolean
+            binding.switch4.isChecked =
+                dataLiveData.value!!.child("/switches/switch4").value as Boolean
+            binding.switch5.isChecked =
+                dataLiveData.value!!.child("switches/switch5").value as Boolean
+            deviceDateTimeStr = dataLiveData.value!!.child("deviceStats/dateTime").value as String
 
-//            val deviceDateTime = dataLiveData.value!!.child("deviceStats/dateTime").value as String
-
-//            Log.i("TAG-DevStats", deviceDateTime)
-//            val elapsedTime = Duration.between(LocalDateTime.now(), LocalDateTime.parse(deviceDateTime)).seconds
-//            val elapsedTime = DateTime.now().difference(DateTime.parse(deviceDateTime))
-//            val currentTimeStamp: Long = Clock.System.now().toEpochMilliseconds()
-//            val deviceTimeStamp = kotlinx.datetime.
-//            Log.i("TAG-Dev-now", currentTimeStamp.toString())
-//            if (elapsedTime.inSeconds > 10) {
-//                _isDeviceOnline.value = false
-//            } else {
-//                _isDeviceOnline.value = true
-//            }
+            checkDeviceStatus()
 
             updateFanSpeed()
         }
 
-        menuBtn.setOnClickListener { view ->
+        binding.menuBtn.setOnClickListener { view ->
             showPopup(view)
         }
 
@@ -157,15 +142,22 @@ class MainActivity : ComponentActivity() {
         dataLiveData.observe(this) { snapshot ->
             // If the data retrieval is successful, hide the progress bar
             if (snapshot != null) {
-                progressBarHolder.animation = outAnimation
-                progressBarHolder.visibility = View.GONE
+                binding.progressBarHolder.animation = outAnimation
+                binding.progressBarHolder.visibility = View.GONE
+            }
+        }
+
+        FirebaseCrud.getDeviceStatus().observe(this) { snapshot ->
+            if (snapshot.key == "dateTime") {
+                deviceDateTimeStr = snapshot.value.toString()
+                checkDeviceStatus()
             }
         }
 
 //        Listen for changes to the fanData
         FirebaseCrud.getFansData().observe(this) { snapshot ->
             if (snapshot.key == "fanSpeed") {
-                fanSpeed.progress = snapshot.value.toString().toInt()
+                binding.fanSpeed.progress = snapshot.value.toString().toInt()
                 updateFanSpeed()
             }
         }
@@ -176,12 +168,12 @@ class MainActivity : ComponentActivity() {
         }
 
 //        Listen for changes to the Fan Speed Control SeekBar
-        fanSpeed.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+        binding.fanSpeed.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 if (fromUser) {
                     if (progress > 0) {
                         fanAnimator.start()
-                        switch5.isChecked = true
+                        binding.switch5.isChecked = true
                     }
                     updateFanSpeed()
                 }
@@ -192,7 +184,7 @@ class MainActivity : ComponentActivity() {
         })
 
 //        Add a listener to the mainSwitch to toggle all the switches
-        mainSwitch.setOnClickListener {
+        binding.mainSwitch.setOnClickListener {
             toggleAllSwitches()
         }
 
@@ -203,20 +195,21 @@ class MainActivity : ComponentActivity() {
                 coroutineScope.launch(coroutineExceptionHandler) {
                     FirebaseCrud.setSwitchStatus(switch.text.toString(), isChecked)
                 }
-                if (isChecked && switch1.isChecked && switch2.isChecked && switch3.isChecked && switch4.isChecked && switch5.isChecked)
-                    mainSwitch.isChecked = true
+                if (isChecked && binding.switch1.isChecked && binding.switch2.isChecked && binding.switch3.isChecked && binding.switch4.isChecked && binding.switch5.isChecked)
+                    binding.mainSwitch.isChecked = true
 
                 if (!isChecked)
-                    mainSwitch.isChecked = false
+                    binding.mainSwitch.isChecked = false
 
-                if (switch5.isChecked && fanSpeed.progress > 0)
+                if (binding.switch5.isChecked && binding.fanSpeed.progress > 0)
                     fanAnimator.start()
 
-                if (!switch5.isChecked) {
+                if (!binding.switch5.isChecked) {
                     fanAnimator.cancel()
                 }
             }
         }
+
     }
 
     //    Check Network Status
@@ -264,19 +257,37 @@ class MainActivity : ComponentActivity() {
         })
     }
 
+    private fun checkDeviceStatus() {
+        if (this::deviceDateTimeStr.isInitialized) {
+            val deviceDT = LocalDateTime.parse(
+                deviceDateTimeStr,
+                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+            )
+            val elapsedDT = Duration.between(deviceDT, LocalDateTime.now())
+
+            if (elapsedDT.seconds > 10) {
+                binding.deviceStatus.text = getString(R.string.device_offline)
+                binding.deviceStatus.setTextColor(Color.RED)
+            } else {
+                binding.deviceStatus.text = getString(R.string.device_online)
+                binding.deviceStatus.setTextColor(Color.GREEN)
+            }
+        }
+    }
+
     //    toggle all the switches on and off by main switch
     private fun toggleAllSwitches() {
         switchMap.values.map { switch ->
-            switch.isChecked = mainSwitch.isChecked
+            switch.isChecked = binding.mainSwitch.isChecked
         }
     }
 
     //    Function to update the fan speed
     private fun updateFanSpeed() {
-        when (fanSpeed.progress) {
+        when (binding.fanSpeed.progress) {
             0 -> {
                 fanAnimator.duration = 0
-                switch5.isChecked = false
+                binding.switch5.isChecked = false
             }
 
             1 -> {
@@ -321,7 +332,21 @@ class MainActivity : ComponentActivity() {
         val inflater: MenuInflater = popup.menuInflater
         inflater.inflate(R.menu.menu, popup.menu)
         popup.setOnMenuItemClickListener { item ->
-            Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+            when (item.itemId) {
+                R.id.deviceConfigBtn -> {
+//                    TODO Add ESPTouch SmartConfiguration
+
+                    Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+//                    val intent = Intent(this, DeviceConfigActivity::class.java)
+//                    startActivity(intent)
+//                    finish()
+                }
+
+                R.id.logoutBtn -> {
+                    firebaseAuth.signOut()  // sign out
+                }
+
+            }
             true
         }
         popup.show()
@@ -330,13 +355,13 @@ class MainActivity : ComponentActivity() {
     //    SHow/Hide network overlay
     private fun showNetworkOverlay(show: Boolean) {
         if (show) {
-            progressBarHolder.animation = outAnimation
-            progressBarHolder.visibility = View.GONE
-            noInternetView.animation = inAnimation
-            noInternetView.visibility = View.VISIBLE
+            binding.progressBarHolder.animation = outAnimation
+            binding.progressBarHolder.visibility = View.GONE
+            binding.noInternetView.animation = inAnimation
+            binding.noInternetView.visibility = View.VISIBLE
         } else {
-            noInternetView.animation = outAnimation
-            noInternetView.visibility = View.GONE
+            binding.noInternetView.animation = outAnimation
+            binding.noInternetView.visibility = View.GONE
         }
     }
 
